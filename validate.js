f67109acd320783dbe945292f229932922eff0fa
veeerpat <phanok45@gmail.com>
2022-03-18 01:54:33 +0700

    Bug fix#2-adding student id validation(id must becomposed of numbersbetween 0 and 9)

--- validate.js
+++ validate.js
@@ -1,13 +1,23 @@
+function isNum(value){
+	return !isNaN(value)
+  }
+
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
   if (sid.length == 10) {
+  if (sid.length == 10&isNum(sid)) {
     return true;
   } else {
     return false;
   }
+  } 
+  	else{
+		return false;
+	}
 }
 
 function checkCandiNo() {
+
   let candi = (document.getElementById("candi").value).trim();
   if (isNaN(candi) || candi < 1) {
     return false;
